8a790983c0c2d5b68116afeb830800de9cc64a5a
nithish9020 <nithishkumar9020@gmail.com>
2024-10-03 18:29:39 +0530

    firebase depolyment setup;aboutus navigate to my profile

--- src/Components/About/Aboutus.jsx
+++ src/Components/About/Aboutus.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import './Aboutus.css'
+import React, { useEffect } from 'react'
+import './Aboutus.css';
+import { useNavigate } from 'react-router-dom';
 
 const Aboutus = () => {
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/profile/nithish9020');
+  },[]);
+
+  
   return (
     <div className='about-container'>
-        <p>Still in progress</p>
+        <p>Still progress</p>
     </div>
   )
 }
